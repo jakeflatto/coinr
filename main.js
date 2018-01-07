62d98d5c4d0f24cc8f5637a4fdc4bfe6f796ba8d
Paul DiLoreto <paul.diloreto@gmail.com>
2018-01-07 05:38:33 -0800

    Working timeouts with Promises

--- main.js
+++ main.js
@@ -1,78 +1,52 @@
-const CryptoAPI = require('cryptocompare');
-global.fetch = require('node-fetch');
-const pg = require('pg');
-const client = new pg.Client({
-	user: 'coinr_admin',
-	host: 'coinr.ctcg3jpd2obd.us-west-2.rds.amazonaws.com',
-	database: 'coinr',
-	password: 'uEB%a*9LTjY1eG4J',
-	port: 5432
-});
-client.connect();
-
+const fillHistory = require('./fillHistory');
 
 // Settings
 const fsymsUSD = [ 'BTC', 'ETH', 'LTC' ]; // For USD loop
 const fsyms = [ 'ADA', 'ARK', 'BCH', 'BTG', 'DASH', 'EOS', 'IOTA', 'LSK', 'LTC', 'NEO', 'QTUM', 'STRAT', 'SUB', 'TRX', 'XLM', 'XMR', 'XRP', 'XVG', 'ZEC' ];
 const tsyms = [ 'BTC', 'ETH' ];
 const exchanges = [ 'Binance', 'CCCAGG' ];
-const limit = 2000;
-const startTimestamp = new Date('1/1/18');
-let nextTimestamp = startTimestamp - (3600*2001);
 
-let currentExchange = exchanges[0];
-let currentFSym = fsyms[0];
-let currentTSym = tsyms[0];
-let currentTimestamp = startTimestamp;
-
-
-CryptoAPI.histoHour(currentFSym, currentTSym, { limit, timestamp: currentTimestamp, exchange: currentExchange })
-.then(data => {
-	let formattedValues = data.map(obj => {
-		let formatObj = {
-			time: obj.time,
-			exchange: currentExchange,
-			traded_with: currentTSym,
-			traded_for: currentFSym,
-			open: obj.open,
-			high: obj.high,
-			low: obj.low,
-			close: obj.close,
-			volume_traded_with: obj.volumeto,
-			volume_traded_for: obj.volumefrom
-		};
-		return formatObj;
-	})
-	.filter(obj => obj.high)
-	.map(obj => {
-		return formatValue(obj);
-	});
-	let query = formatQuery(formattedValues);
-
-	client.query(query, (err, res) => {
-		if(err)
-			console.log('FUCK! SHIT!');
-		else {
-			// TODO probably other shit
-			console.log('Yeee boyy');
-		}
+// Fills out all combinations
+let combinations = [];
+exchanges.forEach(currentExchange => {
+	tsyms.forEach(currentTSym => {
+		fsyms.forEach(currentFSym => {
+			combinations.push({ fsym: currentFSym, tsym: currentTSym, exchange: currentExchange });
+		});
 	});
 });
+fsymsUSD.forEach(currentFSym => {
+	const currentTSym = 'USD';
+	const currentExchange = 'CCCAGG';
+	combinations.push({ fsym: currentFSym, tsym: currentTSym, exchange: currentExchange });
+});
 
-function formatQuery(values) {
-	return `INSERT INTO price_histories_hourly VALUES ${values.join(',')};`;
+// Recursively run through every combination
+function recursivelyRun(index) {
+	return new Promise((resolve, reject) => {
+		let combo = combinations[index];
+		if(!combo)
+			resolve(true);
+		else {
+			fillHistory(combo.fsym, combo.tsym, combo.exchange).then(nextCombo => {
+				recursivelyRun(index+1).then(() => {
+					resolve(true);
+				});
+			}).catch(err => {
+				reject(err);
+			});
+		}
+	});
 }
 
-function formatValue(obj) {
-	// console.log(obj);
-	return `(TIMESTAMP '${new Date(obj.time*1000).toISOString().replace('T',' ').replace('Z','')}',
-	'${obj.exchange}',
-	'${obj.traded_with}',
-	'${obj.traded_for}',
-	${obj.open}::numeric,
-	${obj.high},
-	${obj.low},
-	${obj.close},
-	${obj.volume_traded_with},
-	${obj.volume_traded_for})`.replace(/\s+/g, ' ');
-};
+// Started from the bottom
+recursivelyRun(77).then(finalRes => {
+	console.log('COMPLETELY FINISHED!!');
+	process.exit();
+}).catch(err => {
+	console.error(err);
+	process.exit();
+});
+// Now we're here
+
+
