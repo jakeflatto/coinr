f68eefa88182f500c21c145dbd62a37bde638cd4
Paul DiLoreto <paul@diloreto.com>
2018-01-06 22:19:48 -0800

    Working API

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const CryptoAPI = require('cryptocompare');
+global.fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 
-console.log(
-	CryptoAPI.coinList();
-);
+// Settings
+const fsymsUSD = [ 'BTC', 'ETH', 'LTC' ];
+const fsyms = [ 'ADA', 'ARK', 'BCH', 'BTG', 'DASH', 'EOS', 'IOTA', 'LSK', 'LTC', 'NEO', 'QTUM', 'STRAT', 'SUB', 'TRX', 'XLM', 'XMR', 'XRP', 'XVG', 'ZEC' ];
+const tsyms = [ 'BTC', 'ETH', 'USD' ];
+const exchanges = [ 'Binance', 'CCCAGG' ];
+const limit = 2000;
+const startTimestamp = new Date('1/1/18').getTime() / 1000;
+
+
+
+CryptoAPI.hisToHour().then(data => {
+	let jsonContent = JSON.stringify(data);
+
+	fs.writeFile('test.json', jsonContent, (err) => {
+		console.log('Saved!');
+	});
+});
