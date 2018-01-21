d15b719950d907d22e1d8b79633e24b4f0c3b7b0
Paul DiLoreto <paul.diloreto@gmail.com>
2018-01-21 04:53:15 -0800

    Filtered out old dates

--- scrapeMarketData/scraper.js
+++ scrapeMarketData/scraper.js
@@ -1,16 +1,18 @@
 const tableToJson = require('tabletojson');
 
+// Gets largest table & removes heading
 function scrapeTable(url) {
 	return new Promise((resolve, reject) => {
 		try {
 			tableToJson.convertUrl(url, { useFirstRowForHeadings: true },
-			(tablesAsJson) => resolve(tablesAsJson[0].slice(1)));
+			(tablesAsJson) => resolve(tablesAsJson.sort((a, b) => b.length -a.length)[0].slice(1)));
 		} catch(err) {
 			reject(err);
 		}
 	});
 };
 
+// Gets all tables, unchanged
 function scrapePage(url) {
 	return new Promise((resolve, reject) => {
 		try {
--- scrapeMarketVolume/scrapeArchive.js
+++ scrapeMarketVolume/scrapeArchive.js
@@ -15,42 +15,49 @@ const client = new pg.Client({
 });
 client.connect();
 
-async function archiveIteration(url, coinList) {
-	let table = (await scraper.scrapePage(url))[2].slice(1);
+async function archiveIteration(url, coinList, mostRecent, table) {
+	let rows = (await scraper.scrapeTable(url));
 	let html = await (await fetch(url)).text();
 	let pTags = $('p', html).toArray().map(item => $(item).text());
 	let date = pTags[pTags.length-1].replace('Last updated:', '');
 	date = new Date(date);
-	
-	table = archiveHelper.formatTable(table, date, coinList);
-	await submitPostgres(client, 'coinmarketcap_histories', table);
-	
-	return table.length;
+	if(mostRecent.getTime() >= date.getTime())
+		return 0;
+
+	rows = archiveHelper.formatTable(rows, date, coinList);
+	await submitPostgres(client, table, rows);
+
+	return rows.length;
 }
 
-async function recursivelyRun(timeline, index, coinList, totalRows) {
+async function recursivelyRun(timeline, index, mostRecent, table, coinList, totalRows) {
 	let currentArchive = timeline[index];
 	if(!currentArchive)
 		return totalRows;
 	
 	console.log(`Scraping Archive from ${new Date(currentArchive.time).toUTCString()}`);
-	let rowsAdded = await archiveIteration(currentArchive.url, coinList);
+	let rowsAdded = await archiveIteration(currentArchive.url, coinList, mostRecent, table);
 	totalRows += rowsAdded;
-	console.log(`Rows Added: ${rowsAdded}\nTotal Rows: ${totalRows}\n------------------------`)
-	return await recursivelyRun(timeline, index+1, coinList, totalRows);
+	console.log(`Rows Added: ${rowsAdded}\nTotal Rows: ${totalRows}\n------------------------`);
+	return await recursivelyRun(timeline, index+1, mostRecent, table, coinList, totalRows);
 }
 
 async function scrapeAllArchives() {
+	const table = 'coinmarketcap_histories';
 	const url = 'http://coinmarketcap.com';
 	let timeline = await archiveHelper.getTimeline(url);
+	let mostRecent = (await client.query(`SELECT MAX(time) FROM ${table};`)).rows[0].max;
+	mostRecent = new Date(mostRecent);
+	timeline = timeline.filter(archive => new Date(archive.time).getTime() > mostRecent.getTime());
+	
 	let coinList = await fs.readFile('./combos.json');
 	coinList = JSON.parse(coinList).map(combo => combo.fsym);
-	
-	return await recursivelyRun(timeline, 0, coinList, 0);
+
+	return await recursivelyRun(timeline, 0, mostRecent, table, coinList, 0);
 }
 
 scrapeAllArchives().then(totalRows => {
-	console.log(`FINISHED getting CoinMarketCap Archive!\nTotal Rows Added: ${totalRows}`);
+	console.log(`FINISHED getting CoinMarketCap Archive!\nTotal Rows Added: ${totalRows || 0}`);
 	process.exit();
 }).catch(err => {
 	console.log(err);
